a85a7990b439b77fecf636956209ce74962572f5
IvanMartic01 <ivanmartic311@gmail.com>
2022-08-27 15:47:32 +0200

    Change of memtable
    Instead of size of memtable in memory, now we will use maximal number of
    elements, as parameter for memtable flush.

--- Structures/Memtable/memtable.go
+++ Structures/Memtable/memtable.go
@@ -7,28 +7,26 @@ import (
 
 type Memtable struct {
 	data *skiplist.SkipList
-	size int
+	maxElNumber int
+	curElNumber int
+
 }
 
-const (
-	TOMBSTONE_SIZE = 1
-	TIMESTAMP_SIZE = 16
-)
 
-func New(maxHeight int, size int) *Memtable {
+func New(maxHeight int, maxElNumber int) *Memtable {
 	return &Memtable{
 		data: skiplist.New(maxHeight),
-		size: size,
+		maxElNumber: maxElNumber,
 	}
 }
 
 func (memtable *Memtable) Add(key string, value []byte) (skiplist.Content, error) {
 	// racunanje velicine elementa koji ce se dodati
-	elementSize := len(key) + len(value) + TOMBSTONE_SIZE + TIMESTAMP_SIZE
+	
 
 	// provera da li ima mesta u memtable
-	if memtable.size >= memtable.size+elementSize {
-		memtable.size = memtable.size + elementSize
+	if memtable.maxElNumber > memtable.curElNumber+1 {
+		memtable.curElNumber = memtable.curElNumber + 1
 		return memtable.data.Add(key, value), nil
 	}
 	return skiplist.Content{}, errors.New("max size reached, flush the data")
@@ -36,20 +34,13 @@ func (memtable *Memtable) Add(key string, value []byte) (skiplist.Content, error
 
 func (memtable *Memtable) Delete(key string) (skiplist.Content, error) {
 
-	// preuzimanje elementa koji zelimo da obrisemo
-	content1, err1 := memtable.data.Get(key)
-	if err1 != nil {
-		return skiplist.Content{}, err1
-	}
-
-	// racunanje velicine elementa
-	elementSize := len(key) + len(content1.Value) + TOMBSTONE_SIZE + TIMESTAMP_SIZE
+	// brisanje
 	content2, err2 := memtable.data.Delete(key)
 	if err2 != nil {
 		return skiplist.Content{}, err2
 	}
 	// smanjenje zauzete velicine u memtable
-	memtable.size = memtable.size - elementSize
+	memtable.curElNumber = memtable.curElNumber - 1
 	return content2, nil
 }
 
@@ -59,27 +50,14 @@ func (memtable *Memtable) Get(key string) (skiplist.Content, error) {
 
 func (memtable *Memtable) Update(key string, value []byte) (skiplist.Content, error) {
 
-	// preuzimanje elementa kojeg zelimo da update
-	content, err := memtable.data.Get(key)
+	
+	// preuzimanje elementa kojeg zelimo da update i update
+	updatedElement, err := memtable.data.Update(key,value)
 	if err != nil {
 		return skiplist.Content{}, err
 	}
-	// Trenutna duzina elementa
-	currentSize := len(content.Value) + len(key) + TOMBSTONE_SIZE + TIMESTAMP_SIZE
-
-	// Nova duzina elementa
-	newSize := len(value) + len(key) + TOMBSTONE_SIZE + TIMESTAMP_SIZE
-	sizeDifference := currentSize - newSize
-
-	// provera da li nova vrednost moze stati u memtable
-	if memtable.size >= memtable.size+sizeDifference {
-		updatedElement, err := memtable.data.Update(key, value)
-		if err != nil {
-			return skiplist.Content{}, nil
-		}
-		return updatedElement, nil
-	}
-	return skiplist.Content{}, errors.New("no space in memtable")
+	return updatedElement, nil
+	
 }
 
 func (memtable *Memtable) Serialize() map[string]skiplist.Content {
@@ -87,6 +65,6 @@ func (memtable *Memtable) Serialize() map[string]skiplist.Content {
 }
 
 func (memtable *Memtable) Clear() {
-	memtable.size = 0
+	memtable.curElNumber = 0
 	memtable.data.Clear()
 }
